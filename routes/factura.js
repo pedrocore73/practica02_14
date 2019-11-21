f572c91ef5f1cba0997ba6da761e4631d796a2a9
pedrocore73 <pjimenez@corenetworks.es>
2019-11-21 19:15:01 +0100

    facttt

--- routes/factura.js
+++ routes/factura.js
@@ -7,23 +7,38 @@ app.post('/', (req, res)=>{
     let body = req.body;
     let factura = new Factura({
         cliente: body.cliente,
-        numero: '001-19',
         fecha: body.fecha,
         concepto: body.concepto,
         base: body.base,
         tipo: body.tipo,
         formaPago: body.formaPago
     })
-    factura.save((err, data)=>{
+    Factura.find({}).exec((err, data)=>{
         if(err) {
-            return res.status(400).json({
+            return res.status(500).json({
                 error: err
             })
         }
-        res.status(200).json({
-            mensaje: 'Factura creada correctamente'
+        let ultimaFra;
+        if(data.length > 0) {
+            ultimaFra = (data[data.length - 1]).numero.substring(0,4);
+            ultimaFra = Number(ultimaFra);
+        } else {
+            ultimaFra = 0;
+        }
+        factura.numero = ('0000' + (ultimaFra + 1)).slice(-4) + '-19';
+        factura.save((err, data)=>{
+            if(err) {
+                return res.status(400).json({
+                    error: err
+                })
+            }
+            res.status(200).json({
+                mensaje: 'Factura creada correctamente'
+            })
         })
     })
+
 })
 
 module.exports = app;
