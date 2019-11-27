4df8de89ad2e3297b0381d005cafe359615a4ec9
pedrocore73 <pjimenez@corenetworks.es>
2019-11-27 20:59:37 +0100

    fras

--- routes/factura.js
+++ routes/factura.js
@@ -5,6 +5,25 @@ let fs = require('file-system');
 
 let app = express();
 
+function getRedond(valor, decimales) {
+    let valorRedond;
+    let factor = Math.pow(10, decimales);
+    if(valor < 0) {
+      valorRedond = (Math.round(valor * -1 * factor) / factor) * -1;
+    } else {
+      valorRedond = Math.round(valor * factor) / factor;
+    }
+    return Number(valorRedond);
+}
+
+function getFormat(valor, decimales) {
+    let valorFormat = new Intl.NumberFormat('de-DE', {minimumFractionDigits: decimales}).format(valor) + ' €';
+    return valorFormat;
+}
+
+
+
+
 app.get('/', (req, res)=>{
     Factura.find({}).exec((err, data)=>{
         if(err){
@@ -73,12 +92,78 @@ app.post('/', (req, res)=>{
             });
 
             let cliente = facturaGuardada.cliente.nombre;
+            let calle = facturaGuardada.cliente.direccion.calle;
+            let cp = facturaGuardada.cliente.direccion.cp;
+            let localidad = facturaGuardada.cliente.direccion.localidad;
+            let cif = facturaGuardada.cliente.cif;
+            let email = facturaGuardada.cliente.email;
+            let numero = facturaGuardada.numero;
+            let fechaUTC = new Date(facturaGuardada.fecha);
+            let fecha = fechaUTC.getDate() + '/' + fechaUTC.getMonth() + '/' + fechaUTC.getFullYear();
+            let concepto = facturaGuardada.concepto;
+            let formaPago = facturaGuardada.formaPago;
+            let base = getRedond(facturaGuardada.base, 2);
+ 
+            let tipo = facturaGuardada.tipo;
+ 
+            let importeIVA = getRedond(facturaGuardada.base * facturaGuardada.tipo, 2);
 
+            let total = getRedond(facturaGuardada.base + (facturaGuardada.base * facturaGuardada.tipo), 2);
+
+            let baseFormat = getFormat(base, 2);
+            let tipoFormat = (tipo * 100) + ' %';
+            let importeIVAFormat = getFormat(importeIVA, 2);
+            let totalFormat = getFormat(total, 2);
+            
             let fichero = (`./facturas/${facturaGuardada.numero}.pdf`).toString();
+            doc.margins = { top: 72, bottom: 72, left: 72, right: 72};
 
-            doc.fontSize(14).text(cliente, 200, 100);
+            doc.path("M20.78,25.11,45.88,0l-10,2.73a57.63,57.63,0,0,0-26,14.63L2.16,25.11a13.18,13.18,0,0,0,18.62,0ZM12.4,19.87A53.14,53.14,0,0,1,33.84,7L18.28,22.6a9.65,9.65,0,0,1-10.64,2Z").fillAndStroke("#eb642a");
+            doc.path("M17.36,63.28,25.1,71a13.15,13.15,0,0,0,0-18.61L0,27.3l2.73,10A57.63,57.63,0,0,0,17.36,63.28Zm5.24-8.37a9.63,9.63,0,0,1,2,10.63l-4.77-4.76A53.06,53.06,0,0,1,7,39.34Z").fillAndStroke("#1d2a38");
+            doc.path("M61.75,44.29a13.13,13.13,0,0,0-9.31,3.86l-25.1,25.1,10-2.73a57.63,57.63,0,0,0,26-14.63l7.74-7.74a13.15,13.15,0,0,0-9.3-3.86Zm-.94,9.1A53.12,53.12,0,0,1,39.37,66.22L54.94,50.65a9.65,9.65,0,0,1,10.64-2Z").fillAndStroke("#eb642a");
+            doc.path("M48.11,20.85,73.22,46,70.49,36A57.65,57.65,0,0,0,55.85,10L48.11,2.23A13.17,13.17,0,0,0,48.11,20.85Zm.48-13.14,4.76,4.76A53.14,53.14,0,0,1,66.18,33.91L50.61,18.35A9.65,9.65,0,0,1,48.59,7.71Z").fillAndStroke("#1d2a38");
+
+            doc.font('Helvetica-Bold').fontSize(16).fill('#68676f').text('ACME, S.A.', 415, 60);
+            doc.font('Helvetica').fontSize(14).fill('#68676f').text('Serrano Galvache, 56', 415, 82);
+            doc.fontSize(14).text('28033 Madrid', 415, 104);
+            doc.fontSize(14).text('CIF A12345678', 415, 126);
+
+            doc.fontSize(20).fill('#eb642a').text('Factura', 250, 140);
+
+            doc.rect(40, 170, 515, 120).stroke('#68676f');
+            doc.fontSize(14).fill('#68676f').text(`Cliente: ${cliente}`, 50, 180);
+            doc.fontSize(14).text(`Domicilio: ${calle}`, 50, 202);
+            doc.fontSize(14).text(`C.P.: ${cp} Localidad: ${localidad}`, 50, 224);
+            doc.fontSize(14).text(`CIF: ${cif}`, 50, 246);
+            doc.fontSize(14).text(`Correo electrónico: ${email}`, 50, 268);
+
+            doc.rect(40, 290, 150, 30).stroke('#68676f');
+            doc.rect(190, 290, 365, 30).stroke('#68676f');
+            doc.fontSize(14).text(`Número fra.: ${numero}`, 50, 300);
+            doc.fontSize(14).text(`Fecha: ${fecha}`, 200, 300);
+
+            doc.rect(40, 320, 515, 360).stroke('#68676f');
+            doc.fontSize(14).text(concepto, 50, 330);
+
+            doc.rect(40, 680, 300, 90).stroke('#68676f');
+            doc.fontSize(14).text('Forma de pago:', 50, 690);
+            doc.fontSize(14).text(formaPago, 50, 712);
+
+            doc.rect(340, 680, 215, 30).stroke('#68676f');
+            doc.fontSize(14).text('Base imponible:', 350, 690);
+            doc.fontSize(14).text(baseFormat, 425, 690, {width: 120, align: 'right'});
+
+            doc.rect(340, 710, 215, 30).stroke('#68676f');
+            doc.fontSize(14).text(`IVA ${tipoFormat}:`, 350, 720);
+            doc.fontSize(14).text(importeIVAFormat, 425, 720, {width: 120, align: 'right'});
+
+            doc.rect(340, 740, 215, 30).stroke('#68676f');
+            doc.fontSize(14).text('Total factura:', 350, 750);
+            doc.fontSize(14).text(totalFormat, 425, 750, {width: 120, align: 'right'});
+
 
             doc.end();
+
 
             doc.pipe(fs.createWriteStream(fichero));
 
