fdbb2ee6786f0dae01b13d94e50c26cdef9d10fa
pedrocore73 <pjimenez@corenetworks.es>
2019-11-28 19:01:56 +0100

    mailing

--- routes/email.js
+++ routes/email.js
@@ -1,7 +1,9 @@
 let express = require('express');
 let app = express();
+let nodemailer = require('nodemailer');
 
 let Factura = require('../models/factura');
+let env = require('../env');
 
 app.get('/factura/:id', (req, res)=>{
 
@@ -11,7 +13,32 @@ app.get('/factura/:id', (req, res)=>{
                 error: err
             })
         }
-        console.log(factura);
+        
+        let transporter = nodemailer.createTransport({
+            service: 'gmail',
+            auth: {
+                user: 'japanlenovo19@gmail.com',
+                pass: env.GMAIL_KEY
+            }
+        });
+
+        // Autorizar (solo para Google) desde https://myaccount.google.com/lesssecureapps
+
+        let mailOptions = {
+            from: 'japanlenovo19@gmail.com',
+            to: 'pjimenez@corenetworks.es',
+            subject: 'Test email',
+            html: `<h1>Test de email</h1>`
+        }
+
+        transporter.sendMail(mailOptions, (err, info)=>{
+            if(err) {
+                console.log(err);
+            } else {
+                console.log('Éxito total' + info.response);
+            }
+        })
+
     })
 
 })
